b2cdbc56773ad1c64c457cabbf0acafa3f02c6cb
Jackie Kay <jacquelinekay1@gmail.com>
2017-05-27 12:57:18 +0100

    Stop symlinking between projects

--- include/dispatch/string_literal.hpp
+++ include/dispatch/string_literal.hpp
@@ -1,1 +1,85 @@
-/home/jackie/code/reflection/reflection_experiments/include/reflection_experiments/string_literal.hpp
+#pragma once
+
+#include <array>
+#include <string>
+
+#include <cstring>
+
+#include "utilities.hpp"
+
+namespace dispatch {
+namespace string_literal {
+
+// Heavily inspired by mpark and ubsan
+// For some reason, auto and decltype(auto) crash Clang
+#define CONSTANT(...) \
+  struct { \
+    static constexpr dispatch::string_literal::string_literal value() { return __VA_ARGS__; } \
+  } \
+
+struct string_literal {
+  constexpr string_literal(const char* v, unsigned x) : value(v), s(x) { }
+  constexpr auto size() const { return s; };
+  constexpr auto data() const { return value; };
+  constexpr auto char_at(unsigned i) const { return value[i]; };
+private:
+  const char* value;
+  const unsigned s;
+};
+
+template<auto V>
+struct string_constant {
+  static constexpr auto value() {
+    return string_literal(V, utilities::length(V));
+  };
+};
+
+// what if this just was the string literal struct
+#define STRING_TYPE_DECL(StructName, ...) \
+  struct StructName { \
+    static constexpr dispatch::string_literal::string_literal value() { \
+      return dispatch::string_literal::string_literal(__VA_ARGS__, sizeof(__VA_ARGS__) - 1); \
+    } \
+  };
+
+#define STRING_LITERAL(value) \
+  []() { \
+    using Str = CONSTANT(dispatch::string_literal::string_literal{value, sizeof(value) - 1}); \
+    return Str{}; \
+  }() \
+
+#define STRING_LITERAL_VALUE(value) \
+  dispatch::string_literal::string_literal{value, sizeof(value) - 1}
+
+template<typename Str>
+struct compare_helper {
+  template<size_t... I>
+  static constexpr bool apply(const char* v, std::index_sequence<I...>) {
+    return ((Str::value().char_at(I) == v[I]) && ...);
+  }
+};
+
+template<typename Str>
+static constexpr bool empty(const Str&) {
+  return Str::value().size() == 0;
+}
+
+template<typename Str>
+static constexpr bool equal(const Str&, const Str& b) {
+  return Str::value().data() == b.value().data();
+}
+
+template<typename Str>
+static constexpr bool equal(const Str&, const char* b) {
+  if (utilities::length(b) != Str::value().size()) {
+    return false;
+  } else {
+    return compare_helper<Str>::apply(
+      b, std::make_index_sequence<Str::value().size()>{});
+  }
+}
+
+STRING_TYPE_DECL(empty_string_t, "")
+
+}  // namespace string_literal
+}  // namespace jk
